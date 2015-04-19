dca29e4839046e55930f924f2cf61ff811d1db22
Ravi Rahman <ravi@ravirahman.com>
2015-04-18 20:55:34 -0400

    Initial Commit

---
+++ Milton Mobile iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Milton Mobile iOS
+//
+//  Created by Ravi Rahman on 4/18/15.
+//  Copyright (c) 2015 Milton Academy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

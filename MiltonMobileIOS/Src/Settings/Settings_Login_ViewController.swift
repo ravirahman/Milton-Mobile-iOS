738c5bd920939507eb7af07a5c1329e044edc572
Ravi Rahman <ravi@ravirahman.com>
2015-05-28 22:34:52 -0400

    login data

--- MiltonMobileIOS/Src/Settings/Settings_Login_ViewController.swift
+++ MiltonMobileIOS/Src/Settings/Settings_Login_ViewController.swift
@@ -19,20 +19,42 @@ class Settings_Login_ViewController: UIViewController {
         let url = NSURL(string: "https://my.milton.edu/student/index.cfm") //TODO fix the date so the user can select it
         var request : NSMutableURLRequest = NSMutableURLRequest()
         
-        Alamofire.request(.POST,"https://my.milton.edu/student/index.cfm",parameters: ["":""]).response{(request, response, data, error) in
-
-            
+        Alamofire.request(.POST,"https://my.milton.edu/student/index.cfm").responseString{(_,_,data,_) in
+            var nsdata = NSString(string: data!);
+            var document = HTMLDocument(string: nsdata as String)
+            var blc = document.firstNodeMatchingSelector(".bluelabel").textContent
+            if (blc == nil) {
+                //error
             }
+            var lastName = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter(">")!+9), end: advance(blc.startIndex, blc.indexOfCharacter(",")!)))
+            var firstName = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter(",")!+2), end: advance(blc.startIndex, blc.indexOfCharacter("[")!-1)))
+            var classRoman = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter("[")!+8), end: advance(blc.startIndex, blc.indexOfCharacter(":")!-1)))
+            var classNumber = 0;
+            if (classRoman == "IV") {
+                classNumber = 4;
+            }
+            if (classRoman == "III") {
+                classNumber = 3;
+            }
+            
+            if (classRoman == "II") {
+                classNumber = 2;
+            }
+            if (classRoman == "I") {
+                classNumber = 1;
+            }
+            
+        }
     }
+
     
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
+}
+extension String
+{
+    public func indexOfCharacter(char: Character) -> Int? {
+        if let idx = find(self, char) {
+            return distance(self.startIndex, idx)
+        }
+        return nil
     }
-    */
-    
 }

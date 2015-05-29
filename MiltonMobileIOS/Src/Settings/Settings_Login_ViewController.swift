a325d5332810c30b14250dbebb71061c6547800d
Ravi Rahman <ravi@ravirahman.com>
2015-05-29 12:46:41 -0400

    implementing the keychain

--- MiltonMobileIOS/Src/Settings/Settings_Login_ViewController.swift
+++ MiltonMobileIOS/Src/Settings/Settings_Login_ViewController.swift
@@ -15,33 +15,81 @@ class Settings_Login_ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var accessTypeControler: UISegmentedControl!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func loginClicked(sender: AnyObject) {
+        var accessType = accessTypeControler.titleForSegmentAtIndex(accessTypeControler.selectedSegmentIndex)
+        var username = usernameField.text
+        var password = passwordField.text;
+        if (accessType == "Teacher" || accessType == "Parent") {
+            var alert = UIAlertView();
+            alert.title = "Not Supported"
+            alert.message = accessType! + " login not yet supported"
+            alert.addButtonWithTitle("Dismiss")
+            alert.show()
+        }
+        else {
+            validateCredentials(username, password: password)
+        }
+    }
     func validateCredentials(username: String, password: String) {
-        let url = NSURL(string: "https://my.milton.edu/student/index.cfm") //TODO fix the date so the user can select it
-        var request : NSMutableURLRequest = NSMutableURLRequest()
         
-        Alamofire.request(.POST,"https://my.milton.edu/student/index.cfm").responseString{(_,_,data,_) in
+        Alamofire.request(.POST,"https://my.milton.edu/student/index.cfm", parameters: ["UserLogin": username, "UserPassword": password]).responseString{(_,_,data,_) in
             var nsdata = NSString(string: data!);
             var document = HTMLDocument(string: nsdata as String)
-            var blc = document.firstNodeMatchingSelector(".bluelabel").textContent
-            if (blc == nil) {
-                //error
+            if data?.lowercaseString.rangeOfString("bluelabel") != nil {
+                var blc = document.firstNodeMatchingSelector(".bluelabel").textContent;
+                blc = blc.stringByReplacingOccurrencesOfString("::", withString: "@").stringByReplacingOccurrencesOfString("\n", withString: "").stringByReplacingOccurrencesOfString("\t", withString: "").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ").stringByReplacingOccurrencesOfString("  ", withString: " ")
+                
+                var lastNameStart = advance(blc.startIndex,blc.indexOfCharacter("s")!+9)
+                var lastNameEnd = advance(blc.startIndex, blc.indexOfCharacter(",")!)
+                var lastName = blc.substringWithRange(Range<String.Index>(start: lastNameStart, end: lastNameEnd))
+                
+                
+                
+                var firstNameStart = advance(blc.startIndex,blc.indexOfCharacter(",")!+2)
+                var firstNameEnd = advance(blc.startIndex, blc.indexOfCharacter("[")!-1)
+                var firstName = blc.substringWithRange(Range<String.Index>(start: firstNameStart, end: firstNameEnd))
+                
+                var classRoman = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter("[")!+8), end: advance(blc.startIndex, blc.indexOfCharacter("@")!-1)))
+                var classNumber = 0;
+                if (classRoman == "IV") {
+                    classNumber = 4;
+                }
+                if (classRoman == "III") {
+                    classNumber = 3;
+                }
+                
+                if (classRoman == "II") {
+                    classNumber = 2;
+                }
+                if (classRoman == "I") {
+                    classNumber = 1;
+                }
+                println("username: " + username);
+                println("firstName: " + firstName);
+                println("lastName: " + lastName);
+                println("class: " + String(classNumber));
+                
+                KeychainWrapper.setString(username, forKey: "username")
+                KeychainWrapper.setString(password, forKey: "password")
+                KeychainWrapper.setString(firstName, forKey: "firstName")
+                KeychainWrapper.setString(lastName, forKey: "lastNAme")
+                KeychainWrapper.setString(String(classNumber), forKey: "classNumber")
+                var alert = UIAlertView();
+                alert.title = "Welcome"
+                alert.message = "Welcome " + firstName + " " + lastName
+                alert.addButtonWithTitle("Continue")
+                alert.show()
+                self.navigationController?.popViewControllerAnimated(true)
             }
-            var lastName = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter(">")!+9), end: advance(blc.startIndex, blc.indexOfCharacter(",")!)))
-            var firstName = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter(",")!+2), end: advance(blc.startIndex, blc.indexOfCharacter("[")!-1)))
-            var classRoman = blc.substringWithRange(Range<String.Index>(start: advance(blc.startIndex,blc.indexOfCharacter("[")!+8), end: advance(blc.startIndex, blc.indexOfCharacter(":")!-1)))
-            var classNumber = 0;
-            if (classRoman == "IV") {
-                classNumber = 4;
-            }
-            if (classRoman == "III") {
-                classNumber = 3;
-            }
-            
-            if (classRoman == "II") {
-                classNumber = 2;
-            }
-            if (classRoman == "I") {
-                classNumber = 1;
+            else {
+                var alert = UIAlertView();
+                alert.title = "Check your username and password"
+                alert.message = "These credentials could not be validated"
+                alert.addButtonWithTitle("OK")
+                alert.show()
             }
             
         }

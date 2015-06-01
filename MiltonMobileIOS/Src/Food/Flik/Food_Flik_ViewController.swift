306b521df5c9a7c6d13b73e5f3a0292ef5f99652
Ravi Rahman <ravi@ravirahman.com>
2015-06-01 17:09:13 -0400

    started to link flik

--- MiltonMobileIOS/Src/Food/Flik/Food_Flik_ViewController.swift
+++ MiltonMobileIOS/Src/Food/Flik/Food_Flik_ViewController.swift
@@ -1,37 +1,30 @@
 import UIKit
+import Alamofire
 
 class Food_Flik_ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    var array = Array<String>()
+
     var numTitle: Int = 0;
     var num = 1;
-    var numSectionsBreakfast = 3
-    var numSectionsLunch = 5
-    var numSectionsDinner = 6
     var imageThing : UIImageView?
-
-    
+    var TableData : JSON
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 3
+        var tableAsDictionary = TableData.dictionaryValue
+        return tableAsDictionary.count
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch(section){
-            case 1:
-                return numSectionsLunch
-            case 2:
-                return numSectionsDinner
-            default:
-                return numSectionsBreakfast
-        }
+        var tableAsDictionary = TableData.dictionaryValue
+        var key : String = Array(tableAsDictionary.keys)[section]
+        return tableAsDictionary[key]!.count
     }
     
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = UITableViewCell()
         
-        if (indexPath.section == 0) {
+        /*if (indexPath.section == 0) {
             cell.textLabel?.text = "Breakfast: " + array[indexPath.row]
         }
         if (indexPath.section == 1) {
@@ -47,25 +40,25 @@ class Food_Flik_ViewController: UIViewController, UITableViewDataSource, UITable
         if(indexPath.row % 3 == 0){
             cell.imageView?.image = image
         }
-        
+        */
         return cell;
     }
     
     
     func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        switch(section){
+        /*switch(section){
             case 1:
                 return "Lunch"
             case 2:
                 return "Dinner"
             default:
                 return "Breakfast"
-        }
+        }*/
     }
     
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        switch(indexPath.section){
+        /*switch(indexPath.section){
             case 1:
                 var alert = UIAlertController(title: "Lunch", message: "Selected " + (array[indexPath.row]), preferredStyle: UIAlertControllerStyle.Alert)
                 alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
@@ -106,8 +99,8 @@ class Food_Flik_ViewController: UIViewController, UITableViewDataSource, UITable
                 self.presentViewController(alert, animated: true, completion: nil)
                 break
             
-            default:
-                var alert = UIAlertController(title: "Breakfast", message: "Selected " + (array[indexPath.row]), preferredStyle: UIAlertControllerStyle.Alert)
+            default:*/
+                var alert = UIAlertController(title: indexPath.item "Breakfast", message: "Selected " + (array[indexPath.row]), preferredStyle: UIAlertControllerStyle.Alert)
 
                 alert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { action in
                     switch action.style{
@@ -126,31 +119,30 @@ class Food_Flik_ViewController: UIViewController, UITableViewDataSource, UITable
                     }
                 }))
                 self.presentViewController(alert, animated: true, completion: nil)
-                break;
-        }
+               // break;
+     //   }
 
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = NSURL(string: "http://flik.ma1geek.org/getMeals.php?date=2015-05-22&version=2") //TODO fix the date so the user can select it
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) {(data, response, error) in
-            let json = JSON(data: data)
-            if let mealtimes = json.dictionary {
-                //format of dictionary mealtimes:
-                //{mealtime:{mealclass:[menuitems],mealcalss2:[menuitems]},mealtime2:{mealclass:[menuitems],mealcalss2:[menuitems]}
-                //for example:
-                //{Lunch:{Entree:[Hamburgers, Hot Dogs, French Fries], Flik Live: Caesar Wraps},Dinner:{Entree: [Good ol' plain pasta, Yummy carrot sticks],Dessert:[Pink Ice Cream, Dog Biscuits]}}
-                //TODO Justin please populate the table using this format
-                for (mealtime, categories) in mealtimes {
-                    //mealtimes and categories are SwiftyJSON Objects -- see https://github.com/SwiftyJSON/SwiftyJSON
-                }
-            }
+        Alamofire.request(.GET,"http://flik.ma1geek.org/getMeals.php", parameters:["date":"2015-05-22","version":2]).responseJSON{(_,_,data,_) in
+            self.TableData = JSON(data!);
+            self.populateTableData()
+            //format of dictionary mealtimes:
+            //{mealtime:{mealclass:[menuitems],mealcalss2:[menuitems]},mealtime2:{mealclass:[menuitems],mealcalss2:[menuitems]}
+            //for example:
+            //{Lunch:{Entree:[Hamburgers, Hot Dogs, French Fries], Flik Live: Caesar Wraps},Dinner:{Entree: [Good ol' plain pasta, Yummy carrot sticks],Dessert:[Pink Ice Cream, Dog Biscuits]}}
+            //TODO Justin please populate the table using this format
+                //mealtimes and categories are SwiftyJSON Objects -- see https://github.com/SwiftyJSON/SwiftyJSON
+
         }
-        
-        task.resume()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func populateTableData() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.tableView!.reloadData()
+            return
+        })
     }
 
     override func didReceiveMemoryWarning() {
